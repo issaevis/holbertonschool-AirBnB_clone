4674d2f6ad81f9c4b794d0bbecf5ee30efb831aa
issaevis <evis.topollaj@gmail.com>
2023-07-03 13:50:48 +0200

    Added a couple fixes to base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+'''BaseModel class to build on top of'''
 
 
 from uuid import uuid4
@@ -7,21 +8,23 @@ from datetime import datetime
 
 class BaseModel:
     def __init__(self):
+        '''inits the class'''
         self.id = str(uuid4())
         self.created_at = datetime.now()
-        self.updated_at = self.created_at
-
+        self.updated_at = datetime.now()
 
     def __str__(self):
-        return (f"[{self.name}] ({self.id}) <{self.__dict__}>")
+        '''messes with the str return'''
+        return (f"[{self.__class__.__name__}] ({self.id}) <{self.__dict__}>")
 
     def save(self):
+        '''changes the savedate'''
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        '''adds to a dict and returns it'''
         dictionary = self.__dict__
-        setattr(self, "__class__", self.__class__)
+        setattr(self, "__class__", self.__class__.__name__)
         setattr(self, "created_at", self.created_at.isoformat())
         setattr(self, "updated_at", self.updated_at.isoformat())
         return dictionary
-
