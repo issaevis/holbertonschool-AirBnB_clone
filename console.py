92a443e12867aa003b0c1e1ac5b8291070fe4f82
issaevis <evis.topollaj@gmail.com>
2023-07-04 14:03:52 +0200

    Added the console. It's still quite basic.

--- console.py
+++ console.py
@@ -3,6 +3,24 @@
 
 
 import cmd
+import sys
 
 
 class HBNBCommand(cmd.Cmd):
+    '''Class to create the command line interface of our project.'''
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        '''Quit command to exit the program'''
+        exit()
+
+    def emptyline(self):
+        pass
+
+    def do_EOF(self, arg):
+        """ Keyboard interrupt signal, exit program. """
+        self.do_quit("")
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
