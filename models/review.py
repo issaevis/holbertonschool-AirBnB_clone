8246aa2551498637894515c10f9467a685101eae
issaevis <evis.topollaj@gmail.com>
2023-07-06 17:12:09 +0200

    Added the missing class review.py

---
+++ models/review.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+'''Review Module'''
+
+class State(BaseModel):
+    """Review class"""
+    place_id = ""
+    user_id = ""
+    text = ""
