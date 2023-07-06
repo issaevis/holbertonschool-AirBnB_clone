f880512470817a0a4ff8a1c9f2c03e824ab3e294
issaevis <evis.topollaj@gmail.com>
2023-07-06 15:19:43 +0200

    Seeing if my edits satisfy the checker

--- models/base_model.py
+++ models/base_model.py
@@ -7,20 +7,23 @@ from datetime import datetime
 import models
 
 class BaseModel:
+    '''Base model class that creates basemodel instances'''
+
     def __init__(self, *args, **kwargs):
         '''inits the class'''
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    setattr(self, key, datetime.strptime
-                            (value, "%Y-%m-%dT%H:%M:%S.%f"))
-                if key != "__class__":
-                    setattr(self, key, value)
-        else:
-            self.id = str(uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
+        if not self.__dict__:
+            if kwargs:
+                for key, value in kwargs.items():
+                    if key == "created_at" or key == "updated_at":
+                        value = datetime.strptime
+                        (value, '%Y-%m-%dT%H:%M:%S.%f')
+                    if key != "__class__":
+                        setattr(self, key, value)
+            else:
+                self.id = str(uuid4())
+                self.created_at = datetime.now()
+                self.updated_at = datetime.now()
+                models.storage.new(self)
 
     def __str__(self):
         '''messes with the str return'''
